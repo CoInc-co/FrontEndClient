f42ea7813f9af8df51f6e5a4f5e8465000893859
Scoutzknifez <codygaming@4alexanders.com>
2019-12-23 01:43:59 -0800

    Renamed the css elements to not be applied to all types.

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -12,29 +12,29 @@ class NavBar extends React.Component
         return(
             <div>
                 <nav id = "navBar" className="menu">
-                    <header id = "navHeader">Menu <span>×</span></header>
-                    <ol id = "navElements">
-                        <li className="menu-item"><a href="/">Home</a></li>
-                        <li className="menu-item"><a>Connor Wolanski</a>
+                    <header id = "navHeader" className='header-tab'>Menu <span className='toggle-span'>×</span></header>
+                    <ol id = "navElements" className='sub-menu'>
+                        <li className="menu-item"><a href="/" className='text-format'>Home</a></li>
+                        <li className="menu-item"><a className='text-format'>Connor Wolanski</a>
                             <ol className="sub-menu">
-                                <li className="menu-item"><a href="/">About</a></li>
-                                <li className="menu-item"><a href="/">Resume</a></li>
-                                <li className="menu-item"><a href="/">Projects</a></li>
+                                <li className="menu-item"><a href="/" className='text-format sub-menu-item'>About</a></li>
+                                <li className="menu-item"><a href="/" className='text-format sub-menu-item'>Resume</a></li>
+                                <li className="menu-item"><a href="/" className='text-format sub-menu-item'>Projects</a></li>
                             </ol>
                         </li>
-                        <li className="menu-item"><a>Cody Alexander</a>
+                        <li className="menu-item"><a className='text-format'>Cody Alexander</a>
                             <ol className="sub-menu">
-                                <li className="menu-item"><a href="/">About</a></li>
-                                <li className="menu-item"><a href="/">Resume</a></li>
-                                <li className="menu-item"><a href="/">Projects</a></li>
+                                <li className="menu-item"><a href="/" className='text-format sub-menu-item'>About</a></li>
+                                <li className="menu-item"><a href="/" className='text-format sub-menu-item'>Resume</a></li>
+                                <li className="menu-item"><a href="/" className='text-format sub-menu-item'>Projects</a></li>
                             </ol>
                         </li>
-                        <li className="menu-item"><a href="/">Demo1</a></li>
-                        <li className="menu-item"><a href="/">Demo2</a></li>
-                        <li className="menu-item"><a href="/testapi">API Test Page</a></li>
+                        <li className="menu-item"><a href="/" className='text-format'>Demo1</a></li>
+                        <li className="menu-item"><a href="/" className='text-format'>Demo2</a></li>
+                        <li className="menu-item"><a href="/testapi" className='text-format'>API Test Page</a></li>
                     </ol>
                     <footer>
-                        <button aria-label="Toggle Menu" id= 'navButton'>Toggle</button>
+                        <button aria-label="Toggle Menu" id= 'navButton' className='toggle-button'>Toggle</button>
                     </footer>
                 </nav>
             </div>
@@ -45,7 +45,7 @@ class NavBar extends React.Component
 /**
  * Creates the custom variables needed within the css for the close and open animation.
  */
-function animation(){
+function animation() {
     // Gets the number of items/group sizes of all the components being compressed.
     let count = document.getElementsByTagName("li").length + 1;
     let groupLength = Math.ceil(count / 3);
@@ -84,7 +84,14 @@ function animation(){
         });
 
         // Toggles the classList to be closed for css nav class
-        document.getElementById('navBar').classList.toggle('closed');
+        let elements = document.getElementsByClassName('text-format');
+        for (let i = 0; i < elements.length; i++) {
+            elements.item(i).classList.toggle('closed');
+        }
+
+        document.getElementById('navHeader').classList.toggle('closed');
+        document.getElementById('navButton').classList.toggle('closed');
+
         e.stopPropagation();
     }
 }
