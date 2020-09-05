4da5f4a40f9879359853a3084743ef36a43ce59c
Scoutzknifez <codygaming@4alexanders.com>
2020-09-04 21:40:46 -0700

    Updated the navbar animation

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -29,8 +29,8 @@ class NavBar extends React.Component
                         </ol>
                     </li>
                     <li className="menu-item"><a href="#0">Demo1</a></li>
-                    <li className="menu-item"><a href="/Hailey" className='text-format'>Hailey</a></li>
-                    <li className="menu-item"><a href="/testapi" className='text-format'>API Test Page</a></li>
+                    <li className="menu-item"><a href="/Hailey">Hailey</a></li>
+                    <li className="menu-item"><a href="/testapi">API Test Page</a></li>
                 </ol>
                 <footer>
                     <button aria-label="Toggle Menu" id= 'navButton'>Toggle</button>
@@ -43,22 +43,20 @@ class NavBar extends React.Component
 /**
  * Creates the custom variables needed within the css for the close and open animation.
  */
-function animation() {
-    // Gets the number of items/group sizes of all the components being compressed.
-    let count = document.getElementsByTagName("li").length + 1;
-    let groupLength = Math.ceil(count / 3);
+function animation(){
+    //gets the number of items/group sizes of all the components being compressed.
+    let count = document.getElementsByTagName("li").length+1;
+    let groupLength = Math.ceil(count/3);
     let groupNumber = 0;
     document.getElementById("navElements").style.setProperty('--count', count+'');
-
-    // Gets an array of all the elements being compressed.
+    //gets an array of all the elements being compressed.
     let elements = document.getElementsByTagName("li");
     let elementArray = [];
     elementArray[0] = document.getElementById("navHeader");
     for (let i = 0; i < elements.length; i++){
         elementArray[i+1] = elements[i];
     }
-
-    // Sets what group each element is in.
+    //sets what group each element is in.
     let ind = 1;
     elementArray.forEach(function (entry) {
         if ( ind > groupLength ) {
@@ -68,30 +66,19 @@ function animation() {
         entry.setAttribute('data-group', groupNumber);
         ind++;
     });
-
-    // OnClick function to open and close the navigation menu
+    //on click function to open and close the navigation menu
     document.getElementById('navButton').onclick = function (e) {
         e.preventDefault();
         elementArray.forEach(function (entry) {
-            // Sets top location custom variable for each element for transition
+            //sets top location custom variable for each element for transition
             entry.style.setProperty('--top', entry.getBoundingClientRect().top + (entry.getAttribute('data-group')*-15)-20);
-
             //sets the delay in and out time for each each element
             entry.style.setProperty('--delay-in', elementArray.indexOf(entry)*.05+'s');
             entry.style.setProperty('--delay-out', (count-elementArray.indexOf(entry))*.05+'s');
         });
-
-        // Toggles the classList to be closed for css nav class
-        let elements = document.getElementsByClassName('text-format');
-        for (let i = 0; i < elements.length; i++) {
-            elements.item(i).classList.toggle('closed');
-        }
-
-        document.getElementById('navHeader').classList.toggle('closed');
-        document.getElementById('navButton').classList.toggle('closed');
-
+        //toggles the classList to be closed for css nav class
+        document.getElementById('navBar').classList.toggle('closed');
         e.stopPropagation();
     }
 }
-
 export default NavBar;
